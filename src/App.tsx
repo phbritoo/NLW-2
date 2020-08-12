6312a1674f67904be63c0d3e8f32ff8989324298
Paulo Brito <paulobrito@Marinas-MacBook-Air.local>
2020-08-11 22:43:43 -0300

    Setando css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css'
 
 function App() {
   return (
     <div className="App">
-      
-        Hello World!
+       Hello World!
     </div>
   );
 }
